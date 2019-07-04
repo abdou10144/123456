3d7639a25693244efdfee8c077671e0d1fefcdb4
abdou10144 <52550867+abdou10144@users.noreply.github.com>
2019-07-05 01:52:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`s74`,"http://twitch.tv/s74")
 client.user.setStatus("dnd")
 });
 
